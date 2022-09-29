8774032b8ebae1ccc395f5dcf531d9af356e5344
MiSki <skibamichal@interia.pl>
2022-09-29 16:12:10 +0200

    20

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,16 +1,27 @@
 import React from "react";
+import {useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+	const {sendRequest, status} = useHttp(addQuote);
 	const history = useHistory();
 
+	useEffect( () => {
+		if (status === "completed") {
+			history.push("/quotes");
+		}
+
+	}, [status]);
+
 	const addQuoteHandler = quoteData => {
-		console.log(quoteData.text, quoteData.author);
+		sendRequest(quoteData);
 		history.push("/quotes");
 	};
 
-	return <QuoteForm isLoading={false}
+	return <QuoteForm  isLoading={status === "pending"}
 		onAddQuote={addQuoteHandler}
 	/>;
 };
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
 	{ id: "q1", author: "Max", text: "Learning React is fun!" },
-	{ id: "q2", author: "Maximilian", text: "Learning React is great!" },
-	{ id: "q3", author: "Maximilian", text: "Learning React is awesome!" },
+	{
+		id: "q2",
+		author: "Maximilian",
+		text: "Learning React is great!",
+	},
+	{
+		id: "q3",
+		author: "Maximilian",
+		text: "Learning React is awesome!",
+	},
 	{ id: "q4", author: "Maximilian", text: "Learning React is cool!" },
 ];
 
 const QuoteDetails = () => {
-	const {id: params} = useParams();
-	console.log( params);
+	const { id: params } = useParams();
+	console.log(params);
 
 	const quote = DUMMY_QUOTES.find(el => el.id === params);
 
@@ -20,12 +28,17 @@ const QuoteDetails = () => {
 		return <p>No quote found!</p>;
 	}
 	return (
-		<> 
+		<>
 			<HighlightedQuote author={quote.author}
 				text={quote.text}
 			/>
 			<Route path={`/quotes/${params}/comments`}>
 				<Comments />
+			</Route>
+			<Route exact
+				path={`/quotes/${params}`}
+			>
+				<Link to={`/quotes/${params}/comments`}>Wiew comments</Link>
 			</Route>
 		</>
 	);
